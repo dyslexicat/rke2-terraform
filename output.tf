85b7cecaea6ca5f57a878142cf2b15918cb39c94
Alp Kahvecioglu <alp@kloia.com>
2021-11-11 20:54:09 +0300

    added worker ips in the output

--- output.tf
+++ output.tf
@@ -3,8 +3,12 @@ output "elb_address" {
   description = "The DNS Name of the AWS NLB"
 }
 
-output "public_ip_rke2" {
+output "master_public_ip_rke2" {
   value = aws_spot_instance_request.master_nodes[*].public_ip
-  description = "The public IP of the master node"
+  description = "The public IPs of the master nodes"
 }
 
+output "worker_public_ip_rke2" {
+  value = aws_spot_instance_request.worker_nodes[*].public_ip
+  description = "The public IPs of the worker nodes"
+}
